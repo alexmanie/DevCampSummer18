ddd6c79a78d2fe26bf2408690102de69314f348c
Francisco Javier Abarca Calderín <danielpozoromero@hotmail.com>
2018-07-09 17:02:21 +0200

    FileUtils

--- Daniel Pozo Y Javier Abarca/Lab1/Lab1/Utilities/FileUtils.cs
+++ Daniel Pozo Y Javier Abarca/Lab1/Lab1/Utilities/FileUtils.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +10,53 @@ namespace Lab1.Utilities
 {
     class FileUtils
     {
+        public static object ReadData(string filetoread)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var resourceName = filetoread;
+
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            {
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    var sampleDataText = reader.ReadToEnd();
+
+                    return sampleDataText;
+                }
+            }
+        }
+
+        public static string ReadSampleData(string filetoread)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var resourceName = filetoread;
+
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            {
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    var sampleDataText = reader.ReadToEnd();
+
+                    return sampleDataText;
+                }
+            }
+        }
+
+        public static async Task<string> ReadSampleDataAsync(string filetoread)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var resourceName = filetoread;
+
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            {
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    var sampleDataText = await reader.ReadToEndAsync();
+
+                    return sampleDataText;
+                }
+            }
+        }
+
     }
 }
