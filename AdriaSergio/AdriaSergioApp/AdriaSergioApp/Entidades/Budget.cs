c5a3a96ee3600e95058563a20c4c76cb8ea602ec
adria.raventos <adria.raventos@C0228>
2018-07-09 17:02:29 +0200

    V2

---
+++ AdriaSergio/AdriaSergioApp/AdriaSergioApp/Entidades/Budget.cs
@@ -0,0 +1,63 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdriaSergioApp.Entidades
+{
+    public partial class Budget
+    {
+        [JsonProperty("BudgetId")]
+        public string BudgetId { get; set; }
+
+        [JsonProperty("Description")]
+        public string Description { get; set; }
+
+        [JsonProperty("Amount")]
+        [JsonConverter(typeof(ParseStringConverter))]
+        public long Amount { get; set; }
+
+        [JsonProperty("AmountEUR")]
+        [JsonConverter(typeof(ParseStringConverter))]
+        public long AmountEur { get; set; }
+
+        [JsonProperty("Dedication")]
+        [JsonConverter(typeof(ParseStringConverter))]
+        public long Dedication { get; set; }
+
+        [JsonProperty("Status")]
+        public string Status { get; set; }
+
+        [JsonProperty("Owner")]
+        public string Owner { get; set; }
+
+        [JsonProperty("Account")]
+        [JsonConverter(typeof(ParseStringConverter))]
+        public long Account { get; set; }
+
+        [JsonProperty("Type")]
+        public string Type { get; set; }
+
+        [JsonProperty("Source")]
+        public string Source { get; set; }
+
+        [JsonProperty("OpenDate")]
+        public DateTimeOffset OpenDate { get; set; }
+
+        [JsonProperty("Year")]
+        public long Year { get; set; }
+
+        [JsonProperty("Month")]
+        public long Month { get; set; }
+
+        [JsonProperty("DateKey")]
+        public long DateKey { get; set; }
+    }
+
+    public partial class Budget
+    {
+        public static List<Budget> FromJson(string json) => JsonConvert.DeserializeObject<List<Budget>>(json, AdriaSergioApp.Entidades.Converter.Settings);
+    }
+}
---
+++ AdriaSergio/AdriaSergioApp/AdriaSergioApp/Entidades/Work.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdriaSergioApp.Entidades
+{public partial class Work
+    {
+        [JsonProperty("WorkId")]
+        public long WorkId { get; set; }
+
+        [JsonProperty("EmployeeID")]
+        public string EmployeeId { get; set; }
+
+        [JsonProperty("EmployeeUnit")]
+        public string EmployeeUnit { get; set; }
+
+        [JsonProperty("BudgetId")]
+        public string BudgetId { get; set; }
+
+        [JsonProperty("Status")]
+        public string Status { get; set; }
+
+        [JsonProperty("AmountEUR")]
+        public double AmountEur { get; set; }
+
+        [JsonProperty("Dedication")]
+        public long Dedication { get; set; }
+
+        [JsonProperty("Year")]
+        public long Year { get; set; }
+
+        [JsonProperty("Month")]
+        public long Month { get; set; }
+    }
+
+    public partial class Work
+    {
+        public static List<Work> FromJson(string json) => JsonConvert.DeserializeObject<List<Work>>(json, AdriaSergioApp.Entidades.Converter.Settings);
+   } 
+}
