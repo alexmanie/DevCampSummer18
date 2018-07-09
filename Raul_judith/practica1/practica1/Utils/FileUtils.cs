ed055dadcb79afc07f8e369ffa0f0a6f23535c8a
Radega1993 <rauldearriba@gmail.com>
2018-07-09 16:11:43 +0200

    lectura fichero

---
+++ Raul_judith/practica1/practica1/Utils/FileUtils.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace practica1.Utils
+{
+    public static class FileUtils
+    {
+
+        public static async Task<string> ReadSampleDataAsync()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var resourceName = "Raul_judith.Data.BudgetData.json";
+
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            {
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    var sampleDataText = await reader.ReadToEndAsync();
+
+                    return sampleDataText;
+                }
+            }
+        }
+    }
+}
